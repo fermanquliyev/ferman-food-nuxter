3a616c08dfaa40c67de5d2d72c8869f4df4324ef
fermanquliyev <33841624+fermanquliyev@users.noreply.github.com>
2021-03-21 11:59:33 +0400

    Text shadow added to header

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -14,10 +14,12 @@
   </section>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 .jumbotron {
   background-image: url("~/assets/headerimg.jpg");
   background-size: cover;
   background-position: center;
+  h1, p.lead {
+text-shadow: 1px 0px 8px rgba(89, 89, 89, 1);  }
 }
 </style>
